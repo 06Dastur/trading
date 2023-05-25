bc282053e9f7582fa506c6adbca197c09fa20f36
06Dastur <06dastur@gmail.com>
2023-05-25 05:57:45 +0300

    ikkinchi

--- main.js
+++ main.js
@@ -3,9 +3,28 @@ const title  = document.getElementById('ism');
 const title2  = document.getElementById('ismi');
 
 
-let profile = document.querySelector(".profile");
-let profile_setting = document.querySelector(".nast");
-let back = document.querySelector(".back_img");
+let profile = document.querySelector(".profile"),
+    profile_setting = document.querySelector(".nast"),
+    back = document.querySelector(".back_img"),
+    ikkinchi = document.querySelector(".ikkinchi"),
+    sel = document.querySelector(".sel"),
+    buy = document.querySelector(".buy");
+
+let son = 10;
+function ikkiga(){
+    son--
+    if(son === 0){
+        ikkinchi.classList.add("active")
+    }
+}
+
+sel.onclick = () => {
+    alert("Mablag` yetarli emas!")
+};
+
+buy.onclick = () => {
+    alert("Mablag` yetarli emas!")
+};
 
 profile_setting.onclick = () => {
     profile.classList.add("active");
